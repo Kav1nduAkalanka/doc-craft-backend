3ae31b69eccb8c5b7d91557b80f4adad9ec9ae64
Kavindu Akalanka <akalankakavindu5@gmail.com>
2026-06-18 11:47:50 +0530

    Delete tests directory

--- tests/test_api.py
+++
@@ -1,8 +0,0 @@
-import httpx
-
-# Test sending a POST with empty body
-response = httpx.post("http://localhost:8000/api/documents/123/export/pdf", json={})
-print("JSON Body response:", response.status_code, response.text)
-
-response2 = httpx.post("http://localhost:8000/api/documents/123/export/pdf")
-print("No Body response:", response2.status_code, response2.text)
